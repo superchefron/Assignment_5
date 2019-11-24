579df5aa32bfb33df57eb16fdd3839825a5efa1c
kahronjacques <rjacquessorrell1@student.gsu.edu>
2019-11-24 08:37:47 -0500

    not finished in time so fuck it
    Signed-off-by: kahronjacques <rjacquessorrell1@student.gsu.edu>

--- game.js
+++ game.js
@@ -8,6 +8,28 @@ var time = t.options[t.selectedIndex].value;
 var p = document.getElementById("table_size");
 var size = p.options[p.selectedIndex].value;
 
+if (size === "Level_1"){ // different levels
+    //clearInterval(interval);
+    //startTimer(120);
+    for (var i=1;i<=9;i++){
+        cards.push("image00"+i+".gif");
+    }
+}
+else if (size === "Level_2") {
+    //clearInterval(interval);
+    //startTimer(150);
+    for (var i=1;i<=16;i++){
+        if(i<10)
+            cards.push("image00"+i+".gif");
+        else
+            cards.push("image0"+i+".gif");
+    }
+}
+else {
+    //clearInterval(interval);
+    //startTimer(180);
+}
+
 Array.prototype.memory_tile_shuffle = function (){
     var i = this.length, j, temp;
     while (--i > 0) {
@@ -19,42 +41,22 @@ Array.prototype.memory_tile_shuffle = function (){
 };
 
 function play() {
+    plot_info();
     tiles_flipped = 0;
     var output ='<table>';
-    if (size === "Level_1"){ // different levels
-        //clearInterval(interval);
-        //startTimer(120);
-        for (var i=1;i<=9;i++){
-            cards.push("image00"+i);
-        }
-    }
-    else if (size === "Level_2") {
-        //clearInterval(interval);
-        //startTimer(150);
-        for (var i=1;i<=16;i++){
-            if(i<10)
-                cards.push("image00"+i);
-            else
-                cards.push("image0"+i);
-        }
-    }
-    else {
-        document.getElementById("Level_2").style.display = 'none';
-        document.getElementById("Level_1").style.display = 'none';
-        document.getElementById("Level_3").style.display = 'block';
-        clearInterval(interval);
-        startTimer(180);
-    }
+
     cards.memory_tile_shuffle();
-    var k =1;
-    for (var i=0;i<Math.sqrt(cards.length);i++){
+    var k =0;
+    for (var i=0;i<4;i++){
         output += "<tr>";
-        for (var j=1;j<=Math.sqrt(cards.length);j++) {
-            output += '<td><img src="image00'+k+'" onclick="flip_card(this,\''+cards[k]+'\')"></td>';
+        for (var j=1;j<=4;j++) {
+            output += '<td><img src="'+cards[k]+'" onclick="flip_card(this,\''+cards[k]+'\')" alt="pic"></td>';
+            k++;
         }
-        output += "</tr>"
+        output += "</tr>";
+        k++;
     }
-    document.getElementById("board").innerHTML = output;
+    document.getElementById("board").innerHTML = output+"</table>";
 }
 
 var seconds = 0, minute = 0;
@@ -77,8 +79,8 @@ function startTimer(timeout) {
 }
 
 function plot_info() {
-    document.getElementById("info").innerHTML= "Your goal is to match each of the 8 pictures with its copy, Click the two matching images to reveal them.\n" +
+    document.getElementById("info").innerHTML= cards.length+ "Your goal is to match each of the 8 pictures with its copy, Click the two matching images to reveal them.\n" +
         "Please wait for images to disappear, and then make the next attempt.";
 }
-function flip_card(,val) {
+function flip_card(x,val) {
 }
